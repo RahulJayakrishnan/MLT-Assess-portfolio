84b292c963ab954d4e380aa7ef90f20b3e8683eb
Rahul Jayakrishnan <rahuljayd@gmail.com>
2018-11-07 21:00:42 -0500

    sub

---
+++ analysis.py
@@ -0,0 +1,85 @@
+"""Analyze a portfolio.
+
+Copyright 2017, Georgia Tech Research Corporation
+Atlanta, Georgia 30332-0415
+All Rights Reserved
+"""
+
+import pandas as pd
+import numpy as np
+import datetime as dt
+from scipy.ndimage.interpolation import shift
+from util import get_data, plot_data
+
+# This is the function that will be tested by the autograder
+# The student must update this code to properly implement the functionality
+def assess_portfolio(sd = dt.datetime(2008,1,1), ed = dt.datetime(2009,1,1), \
+    syms = ['GOOG','AAPL','GLD','XOM'], \
+    allocs=[0.1,0.2,0.3,0.4], \
+    sv=1000000, rfr=0.0, sf=252.0, \
+    gen_plot=False):
+
+    # Read in adjusted closing prices for given symbols, date range
+    dates = pd.date_range(sd, ed)
+    prices_all = get_data(syms, dates)  # automatically adds SPY
+    prices = prices_all[syms]  # only portfolio symbols
+    prices_SPY = prices_all['SPY']  # only SPY, for comparison later
+    norm = prices/prices.values[0]
+    allocation = norm * allocs
+    pos_val = allocation * sv
+    port_val = pos_val.sum(axis=1)
+    print type(port_val)
+    cr = (port_val[-1] / port_val[0]) - 1
+    dr = port_val/shift(port_val, 1, cval=np.NaN)-1
+    dr = dr[1:]
+    adr = dr.mean()
+    sddr = dr.std()
+    sr = np.sqrt(sf)*(dr-rfr).mean()/sddr
+    ev = port_val[-1]
+
+    if gen_plot:
+        port_val = port_val/port_val[0]
+
+
+
+        prices_SPY = prices_SPY/prices_SPY[0]
+        df_temp = pd.concat([port_val, prices_SPY], keys=['Portfolio', 'SPY'], axis=1)
+        plot_data(df_temp,title="Daily Portfolio Value against SPY", ylabel='Normalised Price', xlabel='Dates')
+        pass
+
+    return cr, adr, sddr, sr, ev
+
+def test_code():
+    # This code WILL NOT be tested by the auto grader
+    # It is only here to help you set up and test your code
+
+    # Define input parameters
+    # Note that ALL of these values will be set to different values by
+    # the autograder!
+    start_date = dt.datetime(2010,1,1)
+    end_date = dt.datetime(2010,12,31)
+    symbols = ['GOOG', 'AAPL', 'GLD', 'XOM']
+    allocations = [0.2, 0.3, 0.4, 0.1]
+    start_val = 1000000  
+    risk_free_rate = 0.0
+    sample_freq = 252
+
+    # Assess the portfolio
+    cr, adr, sddr, sr, ev = assess_portfolio(sd = start_date, ed = end_date,\
+        syms = symbols, \
+        allocs = allocations,\
+        sv = start_val, \
+        gen_plot = True)
+
+    # Print statistics
+    print "Start Date:", start_date
+    print "End Date:", end_date
+    print "Symbols:", symbols
+    print "Allocations:", allocations
+    print "Sharpe Ratio:", sr
+    print "Volatility (stdev of daily returns):", sddr
+    print "Average Daily Return:", adr
+    print "Cumulative Return:", cr
+
+if __name__ == "__main__":
+    test_code()
